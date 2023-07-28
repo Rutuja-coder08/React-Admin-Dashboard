fdc2f3c1b2c769dd8757ac5050f37bccaa41e4f9
Rutuja Jadhav <Rutuja.Jadhav@BRILLIO.COM>
2023-07-28 18:02:55 +0530

    New branch

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Login from "./pages/login/Login";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+      ]
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default App;
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,6 @@
+const Login = () =>{
+    return(
+        <div className="login">Login</div>
+    )
+}
+export default Login
