6a373f63dd73fad1c9434b376ad71561135c4393
Rutuja Jadhav <Rutuja.Jadhav@BRILLIO.COM>
2023-08-10 15:20:04 +0530

    PieChartBox

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,13 +3,14 @@ import TopBox from "../../components/topBox/TopBox"
 import ChartBox from "../../components/chartBox/ChartBox"
 import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from "../../data"
 import BarChartBox from "../../components/barChartBox/BarChartBox"
+import PieChartBox from "../../components/pieChartBox/PieChartBox"
 const Home = () =>{
     return(
         <div className="home">
             <div className="box box1"><TopBox/></div>
             <div className="box box2"><ChartBox {...chartBoxUser}/></div>
             <div className="box box3"><ChartBox {...chartBoxProduct}/></div>
-            <div className="box box4"></div>
+            <div className="box box4"><PieChartBox/></div>
             <div className="box box5"><ChartBox {...chartBoxConversion}/></div>
             <div className="box box6"><ChartBox {...chartBoxRevenue}/></div>
             <div className="box box7">Box7</div>
